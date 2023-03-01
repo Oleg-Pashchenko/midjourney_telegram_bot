1cf21ee252e0a8d665ec6902ddec5e898a3569a0
Oleg-Pashchenko <pickpar@ya.ru>
2023-03-01 11:14:32 +0300

    update

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-"https://github.com/Wildric-Auric/MidJourney-Wrapper/blob/main/Salai.py"
+
+import replicate
+
+model = replicate.models.get("tstramer/midjourney-diffusion")
+version = model.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
+
+# https://replicate.com/tstramer/midjourney-diffusion/versions/436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b#input
+inputs = {
+    # Input prompt
+    'prompt': "Поэт Александр Сергеевич Пушкин на параде ЛГБТ",
+    'width': 576,
+    'height': 1024,
+    'prompt_strength': 0.8,
+    'num_outputs': 1,
+    'num_inference_steps': 50,
+    'guidance_scale': 7.5,
+    'scheduler': "DPMSolverMultistep",
+
+    # Random seed. Leave blank to randomize the seed
+    # 'seed': ...,
+}
+import time
+start = time.time()
+# https://replicate.com/tstramer/midjourney-diffusion/versions/436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b#output-schema
+output = version.predict(**inputs)
+print(output)
+print(time.time() - start)
